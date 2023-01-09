c0431b044dc1f3d6aaa9628f4b82595da449cd23
Your Name <ndimant@hotmail.com>
2023-01-09 12:22:28 +0200

    importing all components to main page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
-import Navbar from './components/Navbar';
+import React from "react";
+import Announcement from "../components/Announcement";
+import Categories from "../components/Categories";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import Products from "../components/Products";
+import Slider from "../components/Slider";
 import './App.css';
 
 function App() {
